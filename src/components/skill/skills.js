08ed10278fec8e82b0ad531c27cc535aa2bf0769
aonayinka <aonayinka@gmail.com>
2023-12-19 22:05:34 +0000

    2nd

--- src/components/skill/skills.js
+++ src/components/skill/skills.js
@@ -4,6 +4,9 @@ import './skills.css'
 const Skills = () => {
   return (
     <section id='skills'>
+      <button popovertarget = "my-popover">  open </button>
+
+      <div id='my-popover' popover> hello well ocme to balbal </div>
         
     </section>
   )
